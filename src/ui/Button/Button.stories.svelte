3e721c6a32e12af8c7ecf736d8b6deaf212a20a5
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2025-11-25 21:02:17 +0300

    Добавил больше стилей для кнопки

--- src/ui/Button/Button.stories.svelte
+++ src/ui/Button/Button.stories.svelte
@@ -15,9 +15,15 @@
   </Mock>
 </Story>
 
-<Story name="Secondary">
+<Story name="Variants">
   <Mock>
-    <button class="secondary">Button</button>
+    <div class="flex items-center gap-3">
+      <button>Button</button>
+      <button class="secondary">Button</button>
+      <button class="ghost">Button</button>
+      <button class="warn">Button</button>
+      <button class="error">Button</button>
+    </div>
   </Mock>
 </Story>
 
@@ -32,6 +38,21 @@
       <button class="secondary small">Button</button>
       <button class="secondary">Button</button>
       <button class="secondary big">Button</button>
+    </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="ghost small">Button</button>
+      <button class="ghost">Button</button>
+      <button class="ghost big">Button</button>
+    </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="warn small">Button</button>
+      <button class="warn">Button</button>
+      <button class="warn big">Button</button>
+    </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="error small">Button</button>
+      <button class="error">Button</button>
+      <button class="error big">Button</button>
     </div>
   </Mock>
 </Story>
@@ -48,6 +69,21 @@
       <button class="secondary"><IconCheck />Button<IconCheck /></button>
       <button class="secondary big"><IconCheck />Button<IconCheck /></button>
     </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="ghost small"><IconCheck />Button<IconCheck /></button>
+      <button class="ghost"><IconCheck />Button<IconCheck /></button>
+      <button class="ghost big"><IconCheck />Button<IconCheck /></button>
+    </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="warn small"><IconCheck />Button<IconCheck /></button>
+      <button class="warn"><IconCheck />Button<IconCheck /></button>
+      <button class="warn big"><IconCheck />Button<IconCheck /></button>
+    </div>
+    <div class="flex gap-4 items-center mt-5">
+      <button class="error small"><IconCheck />Button<IconCheck /></button>
+      <button class="error"><IconCheck />Button<IconCheck /></button>
+      <button class="error big"><IconCheck />Button<IconCheck /></button>
+    </div>
   </Mock>
 </Story>
 
@@ -55,5 +91,8 @@
   <Mock>
     <button disabled><IconCheck />Button</button>
     <button class="secondary" disabled><IconCheck />Button</button>
+    <button class="ghost" disabled><IconCheck />Button</button>
+    <button class="warn" disabled><IconCheck />Button</button>
+    <button class="error" disabled><IconCheck />Button</button>
   </Mock>
 </Story>
