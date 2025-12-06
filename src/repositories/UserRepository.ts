031d8140a5d9b570694ab1a361daaf907705844b
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2025-12-06 18:31:02 +0300

    Добавил инвалидацию по тегу

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,7 +2,7 @@ import { sql, redis } from 'bun';
 import type { IUserEntity } from '$src/repositories/entities';
 
 export abstract class UserRepository {
-  private static readonly USERS_TAG = 't-users';
+  private static readonly USERS_KEY = 'users';
 
   static async getUserById(id: IUserEntity['id']): Promise<Maybe<IUserEntity>> {
     const cacheKey = `user:${id}`;
@@ -16,8 +16,13 @@ export abstract class UserRepository {
     const [user]: IUserEntity[] = await sql`select * from users where id = ${id}`;
 
     redis.setex(cacheKey, 360, JSON.stringify(user));
-    redis.sadd(cacheKey, UserRepository.USERS_TAG);
+    redis.sadd(cacheKey, UserRepository.USERS_KEY);
 
     return user;
+  }
+
+  static async invalidateUsers(): Promise<void> {
+    const cachedTags = await redis.smembers(UserRepository.USERS_KEY);
+    await redis.del(...cachedTags);
   }
 }
