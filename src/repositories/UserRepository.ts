6a276f09f299b51b5a9381340274db1faf36eae2
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2025-12-06 16:54:39 +0300

    Добавил базовый функционал redis в репозиторий

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,10 +1,23 @@
-import { sql } from 'bun';
+import { sql, redis } from 'bun';
 import type { IUserEntity } from '$src/repositories/entities';
 
 export abstract class UserRepository {
+  private static readonly USERS_TAG = 't-users';
+
   static async getUserById(id: IUserEntity['id']): Promise<Maybe<IUserEntity>> {
-    const users: IUserEntity[] = await sql`select * from users where id = ${id}`;
-    console.log(users);
-    return users[0];
+    const cacheKey = `user:${id}`;
+    const cached = await redis.exists(cacheKey);
+
+    if (cached) {
+      const cachedUser = await redis.get(cacheKey);
+      return cachedUser && JSON.parse(cachedUser);
+    }
+
+    const [user]: IUserEntity[] = await sql`select * from users where id = ${id}`;
+
+    redis.setex(cacheKey, 360, JSON.stringify(user));
+    redis.sadd(cacheKey, UserRepository.USERS_TAG);
+
+    return user;
   }
 }
